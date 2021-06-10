8e4fd7b4ca6b2c25e72f2495151e4a3d344f1310
github username <60378440+vmar13@users.noreply.github.com>
2021-06-10 19:41:53 -0400

    added placeholder head shot and title

--- src/components/image.js
+++ src/components/image.js
@@ -16,7 +16,7 @@ import Img from "gatsby-image"
 const Image = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+      placeholderImage: file(relativePath: { eq: "VM head shot.png" }) {
         childImageSharp {
           fluid(maxWidth: 300) {
             ...GatsbyImageSharpFluid
