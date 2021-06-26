8dd0dc9dd64e0884c2ba3891e519d452218e75d2
github username <60378440+vmar13@users.noreply.github.com>
2021-06-26 14:15:20 -0400

    added header and adjusted positioning of text on workjs

--- src/pages/work.js
+++ src/pages/work.js
@@ -5,7 +5,8 @@ const Work  = () => {
     return (
         <>
         <Navbar />
-        <div>
+        <div className='about-container'>
+            <h1>My Work</h1>
             <h3>F in Tea</h3>
             <h3>Unwind</h3>
             <h3>Recipekeeper</h3>
