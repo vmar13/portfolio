f4a7ad00c30360832677b3bab5b45d0fa26a23b1
github username <60378440+vmar13@users.noreply.github.com>
2021-06-27 22:54:10 -0400

    added F in Tea GIF to workjs

--- src/pages/work.js
+++ src/pages/work.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from "../components/navbar";
+import HnetGIF from '../images/Hnet-image.gif';
 
 const Work  = () => {
     return (
@@ -8,6 +9,7 @@ const Work  = () => {
         <div className='about-container'>
             <h1>My Work</h1>
             <h3>F in Tea</h3>
+            <img src={HnetGIF} alt='F in Tea GIF' />
             <h3>Unwind</h3>
             <h3>Recipekeeper</h3>
         </div> 
