cf0ca078cb717eb05edf0e7bf5b5c077afb7e27e
github username <60378440+vmar13@users.noreply.github.com>
2021-06-28 20:25:28 -0400

    created blog1js and added it to blogjs and adjusted size

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from '../components/navbar';
+import Blog1 from '../components/blog1';
 
 const Blog = () => {
     return (
         <>
             <Navbar />
             <h2 className='about-container'>Blog</h2>
+            <Blog1 />
         </>
     )
 }
