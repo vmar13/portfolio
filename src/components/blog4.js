e7adbc33255934dccdf1a34bef6133667b4127be
Vanessa Martinez <60378440+vmar13@users.noreply.github.com>
2021-08-17 14:34:51 -0400

    added blog4

---
+++ src/components/blog4.js
@@ -0,0 +1,47 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+/*
+ * This component is built using `gatsby-image` to automatically serve optimized
+ * images with lazy loading and reduced file sizes. The image is loaded using a
+ * `useStaticQuery`, which allows us to load the image from directly within this
+ * component, rather than having to pass the image data down from pages.
+ *
+ * For more information, see the docs:
+ * - `gatsby-image`: https://gatsby.dev/gatsby-image
+ * - `useStaticQuery`: https://www.gatsbyjs.com/docs/use-static-query/
+ */
+
+const Blog4 = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "georg-bommeli-EgVXjo_K9go-unsplash.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
+  const styles = {
+    position: `fixed`,
+    top: `65%`,
+    left: `35%`,
+    width: `310px`,
+    height: `250px`,
+    marginLeft: `18%`, 
+    marginRight: `41%`,
+    border: `solid green 5px`
+  }
+
+  if (!data?.placeholderImage?.childImageSharp?.fluid) {
+    return <div>Picture not found</div>
+  }
+
+  return <Img style={styles} fluid={data.placeholderImage.childImageSharp.fluid} />
+}
+
+export default Blog4;
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -3,6 +3,7 @@ import Navbar from '../components/navbar';
 import Blog1 from '../components/blog1';
 import Blog2 from '../components/blog2';
 import Blog3 from '../components/blog3';
+import Blog4 from '../components/blog4';
 
 
 const Blog = () => {
@@ -13,6 +14,7 @@ const Blog = () => {
             <Blog1 />
             <Blog2 />
             <Blog3 />
+            <Blog4 />
         </>
     )
 }
